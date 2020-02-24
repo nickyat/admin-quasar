be47513ecdbaa8574866fd8044b6d88e3618ae5f
Marcos Rativa <marcosr@imaginacolombia.com>
2020-02-24 14:49:53 -0500

    +Delete: morris chart

--- src/boot/client-side.js
+++ src/boot/client-side.js
@@ -1,6 +1,5 @@
 import {Chart} from 'highcharts-vue';
 import { Carousel, Slide } from 'vue-carousel';
-import { DonutChart, BarChart, LineChart, AreaChart } from 'vue-morris'
 import Raphael from 'raphael/raphael'
 global.Raphael = Raphael
 
@@ -10,10 +9,6 @@ export default async ({app, router, store, Vue}) => {
       {name: 'Carousel', component: Carousel},
       {name: 'Slide', component: Slide},
       {name: 'chart', component: Chart},
-      {name: 'donutChart', component: DonutChart},
-      {name: 'barChart', component: BarChart},
-      {name: 'lineChart', component: LineChart},
-      {name: 'areaChart', component: AreaChart},
    ]
 
    //Register components
